bb5664eb0e2a422377b4cca19b0a5eacf0958eb3
Rajiv Mounguengue <rajiv.mounguengue@hotmail.fr>
2024-04-06 13:33:01 +0000

    chore: wip

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,52 @@
-import { z } from 'zod'
-import { Hono } from 'hono'
-import { serve } from '@hono/node-server'
-import { zValidator } from '@hono/zod-validator'
+import { serve } from "@hono/node-server";
+import { zValidator } from "@hono/zod-validator";
+import { Hono } from "hono";
+import { z } from "zod";
 
-const app = new Hono().basePath('v1')
+const app = new Hono().basePath("v1");
 
 const User = z.object({
-  first_name: z.string().optional(),
-  last_name: z.string().min(1, 'At least 1 char').max(50),
-  gender: z.enum(['MALE', 'FEMALE'])
-})
+	first_name: z.string().optional(),
+	last_name: z.string().min(1, "At least 1 char").max(50),
+	gender: z.enum(["MALE", "FEMALE"]),
+});
 
-type User = z.infer<typeof User>
+type User = z.infer<typeof User>;
 
-const users = new Set<User>()
+const users = new Set<User>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+	return c.text("Hello Hono!");
+});
 
-app.get('/users', (c) => {
-  return c.json(Array.from(users))
-})
+app.get("/users", (c) => {
+	return c.json(Array.from(users));
+});
 
-app.post('/users', zValidator('json', User),
-async (c) => {
-  const input = await c.req.json()
+app.post("/users", zValidator("json", User), async (c) => {
+	const input = await c.req.json();
 
-  // const validateResult = await User.safeParseAsync(input)
+	// const validateResult = await User.safeParseAsync(input)
 
-  // if(!validateResult.success) {
-  //   return c.json({
-  //     message: `${validateResult.error.errors[0].path} ==> ${validateResult.error.errors[0].message}`
-  //   }, 400)
-  // }
+	// if(!validateResult.success) {
+	//   return c.json({
+	//     message: `${validateResult.error.errors[0].path} ==> ${validateResult.error.errors[0].message}`
+	//   }, 400)
+	// }
 
-  // const user = validateResult.data
+	// const user = validateResult.data
 
-  const user: User = input
+	const user: User = input;
 
-  users.add(user)
+	users.add(user);
+  
+	return c.json(null);
+});
 
-  return c.json(null)
-})
-
-const port = 3000
-console.log(`Server is running on port ${port}`)
+const port = 3000;
+console.log(`Server is running on port ${port}`);
 
 serve({
-  fetch: app.fetch,
-  port
-})
+	fetch: app.fetch,
+	port,
+});
